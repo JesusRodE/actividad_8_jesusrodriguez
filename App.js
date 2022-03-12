3582cea763680c54ec560829a443b65bc0b60356
Jesus Rodriguez <jesuse1661@gmail.com>
2022-03-11 21:45:59 -0700

    Programa para extaraer archivos de api

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
+import React,{useEffect, useState}from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
 
 export default function App() {
+
+  const[Data, setData] = useState([]);
+  const[Isloading, setLoading] = useState(true);
+
+  const getPosts = async() =>{
+   
+  try{
+    const url ="https://jsonplaceholder.typicode.com/posts";
+    const response = await fetch(url);     //consumir los datos
+    const json = await response.json(); //convertir a json
+    setData(json);
+    } catch(error){
+      console.error(error);
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(()=>{
+    getPosts();
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    {Isloading ? <ActivityIndicator/> : ( 
+      
+      <FlatList 
+        data={Data}
+        keyExtractor = { ({ id }, index) => id }
+        renderItem = { 
+          ({item}) => ( 
+            <Text>{item.title}</Text>
+          )
+        }
+      />
+    )}
     </View>
   );
 }
